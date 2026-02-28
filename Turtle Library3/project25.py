230da279d42e8879429a72c62221b6f395e3a1ac
yiğit kandemir <kandemirryigit@gmail.com>
2026-02-28 08:09:51 -0800

    Add files via upload

---
+++ Turtle Library3/project25.py
@@ -0,0 +1,15 @@
+import turtle as t
+from turtle import Screen
+import random
+
+tim=t.Turtle()
+tim.speed(20)
+
+for i in range(40):
+    tim.circle(60)
+    tim.left(10)
+
+
+
+my_screen=Screen()
+my_screen.exitonclick()
